9a6d55887ba3a32af443e1580870eb8acc3e27e8
Utkarsh Joshi <utkarsh.joshi@sap.com>
2025-03-22 22:20:53 +0530

    Ignite the Swiggy App

---
+++ data/restaurants.js
@@ -0,0 +1,381 @@
+const restaurants = [
+    {
+    "info": {
+    "id": "151656",
+    "name": "Dev International",
+    "cloudinaryImageId": "enj3srsnhbltbom2ovvh",
+    "locality": "khajri road",
+    "areaName": "Mohan Nagar",
+    "costForTwo": "₹100 for two",
+    "cuisines": [
+    "North Indian",
+    "Chinese",
+    "Fast Food",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "parentId": "71556",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "464",
+    "sla": {
+    "deliveryTime": 43,
+    "lastMileTravel": 11.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "40-45 mins",
+    "lastMileTravelString": "11.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-22 22:30:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹125 OFF",
+    "subHeader": "ABOVE ₹349",
+    "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-f860cbd9-9edb-40ed-9518-0adc2c516b28"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/dev-international-khajri-road-mohan-nagar-rest151656",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "658210",
+    "name": "The Fusion Lounge",
+    "cloudinaryImageId": "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+    "locality": "Triloki nagar",
+    "areaName": "Railway Station",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "South Indian",
+    "Chinese",
+    "Beverages",
+    "Fast Food",
+    "Desserts"
+    ],
+    "avgRating": 4.2,
+    "parentId": "395453",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "326",
+    "sla": {
+    "deliveryTime": 45,
+    "lastMileTravel": 10.2,
+    "serviceability": "SERVICEABLE",
+    "slaString": "45-50 mins",
+    "lastMileTravelString": "10.2 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-22 23:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹50 OFF",
+    "subHeader": "ABOVE ₹129",
+    "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-f860cbd9-9edb-40ed-9518-0adc2c516b28"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/the-fusion-lounge-triloki-nagar-railway-station-rest658210",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "151518",
+    "name": "Bakery World",
+    "cloudinaryImageId": "mt2aggiscfl3yviatwng",
+    "locality": "Parasia Road",
+    "areaName": "Parasia Road",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Bakery",
+    "Ice Cream",
+    "Snacks",
+    "Beverages"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "40363",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "260",
+    "sla": {
+    "deliveryTime": 36,
+    "lastMileTravel": 11.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "35-40 mins",
+    "lastMileTravelString": "11.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-22 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-f860cbd9-9edb-40ed-9518-0adc2c516b28"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/bakery-world-parasia-road-rest151518",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "151649",
+    "name": "Hotel Sai Nath & Sai Restaurant",
+    "cloudinaryImageId": "vkhcohhmqfczycw9vsar",
+    "locality": "railway station",
+    "areaName": "Chhindwara Locality",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "North Indian",
+    "South Indian",
+    "Chinese",
+    "Beverages",
+    "Fast Food",
+    "Desserts"
+    ],
+    "avgRating": 4.3,
+    "veg": true,
+    "parentId": "101802",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "976",
+    "sla": {
+    "deliveryTime": 38,
+    "lastMileTravel": 10.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "35-40 mins",
+    "lastMileTravelString": "10.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-22 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-f860cbd9-9edb-40ed-9518-0adc2c516b28"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/hotel-sai-nath-and-sai-restaurant-railway-station-chhindwara-locality-rest151649",
+    "type": "WEBLINK"
+    }
+    },
+    {
+    "info": {
+    "id": "234875",
+    "name": "Adil Hotel",
+    "cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+    "locality": "Rautha Wada",
+    "areaName": "Chhindwara Locality",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "North Indian",
+    "Biryani",
+    "Tandoor"
+    ],
+    "avgRating": 4.3,
+    "parentId": "27123",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1.2K+",
+    "sla": {
+    "deliveryTime": 41,
+    "lastMileTravel": 11.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "40-45 mins",
+    "lastMileTravelString": "11.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-23 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹129"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-f860cbd9-9edb-40ed-9518-0adc2c516b28"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/adil-hotel-rautha-wada-chhindwara-locality-rest234875",
+    "type": "WEBLINK"
+    }
+    }
+]
+
+export default restaurants;
